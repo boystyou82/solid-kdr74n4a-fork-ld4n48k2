5aab78cf79a2aa2e4ede74721be846d3933f6d5e
boystyou82(유성태) <73485664+undefined@users.noreply.github.com>
2023-01-20 14:55:39 +0000

    First commit

---
+++ src/index.stories.js
@@ -0,0 +1,11 @@
+import "./index.js";
+import { html } from "lit-html";
+
+export default {
+  title:'asdf',
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export const story1 = () => html` <my-counter></my-counter> `;
